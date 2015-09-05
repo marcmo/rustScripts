f19b44673419fe50c284319cb7174df2098767a2
Oliver Mueller <oliver.mueller@esrlabs.com>
2015-09-05 16:27:16 +0200

    correct mru path and error handling
    do not delete file before reading it!

--- clean_vim_mru_file.rs
+++ clean_vim_mru_file.rs
@@ -7,40 +7,46 @@ use std::fs;
 use std::vec::Vec;
 use std::error::Error;
 
-fn does_file_exist(file: &str) -> bool {
-    std::fs::metadata(Path::new(file)).map(|m| m.is_file()).unwrap_or(false)
+static BACKUP_FILE: &'static str = ".vim_mru_files_backup";
+static MRU_FILE: &'static str = ".vim_mru_files";
+
+fn does_file_exist(path: &Path) -> bool {
+    std::fs::metadata(path).map(|m| m.is_file()).unwrap_or(false)
 }
 fn get_cleaned_content(mru_path : &Path) -> Result<(String, i32), io::Error> {
     let mut cleaned : Vec<String> = Vec::new();
     let mut rm_cnt = 0;
-    let content = BufReader::new(try!(File::open(&mru_path)));
-    for line in content.lines().filter_map(|result| result.ok()) {
-
+    let reader = BufReader::new(try!(File::open(mru_path)));
+    for line in reader.lines().filter_map(|result| result.ok()) {
         let s = &line[..];
-        if (!line.starts_with('#')) && !does_file_exist(s) {
+        if (!line.starts_with('#')) && !does_file_exist(Path::new(s)) {
             print!("{} ==> does not exists, CLEAN\n", line);
             rm_cnt = rm_cnt + 1;
         } else {
             cleaned.push(line.to_string());
         }
     }
-    let firstline : String = cleaned[0].to_string();
-    let joined : String = cleaned[1..].iter().fold(firstline, |r, c| r + "\n" + c);
-    Ok((joined, rm_cnt))
+    if cleaned.len() > 0 {
+        let firstline : String = cleaned[0].to_string();
+        let joined : String = cleaned[1..].iter().fold(firstline, |r, c| r + "\n" + c);
+        return Ok((joined, rm_cnt));
+    }
+    Err(io::Error::new(io::ErrorKind::Other, "empty cleaned stuff"))
 }
 
 fn main() {
     fn replace_original() -> Result<i32, io::Error> {
-        let mru_file = ".vim_mru_files";
-        if !does_file_exist(mru_file) {
+        use std::env;
+        let home = env::home_dir().expect("homedir not available");
+        let mru_path_buf = Path::new(&home).join(MRU_FILE);
+        let mru_path = mru_path_buf.as_path();
+        if !does_file_exist(mru_path) {
             return Err(io::Error::new(io::ErrorKind::NotFound,
-                                      format!("{} seems not to exist", mru_file)))
+                                      format!("{} seems not to exist", mru_path.display())))
         }
-        let mru_path = Path::new(mru_file);
-        let backup_file = ".vim_mru_files_backup";
-        try!(fs::copy(mru_file, backup_file));
-        let mut file = try!(File::create(&mru_path));
-        let (joined, rm_cnt) = try!(get_cleaned_content(mru_path));
+        try!(fs::copy(mru_path, BACKUP_FILE));
+        let (joined, rm_cnt) = try!(get_cleaned_content(&mru_path));
+        let mut file = try!(File::create(mru_path));
         try!(file.write_all(joined.as_bytes()));
         Ok(rm_cnt)
     }
